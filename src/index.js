6194d8895baec9ebd1739b438f0f75a18ef79338
AHMED <ahmed@localhost.localdomain>
2019-11-05 08:37:43 -0500

    adding header component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
-import { BrowserRouter} from 'react-router-dom';
 
-ReactDOM.render( 
-    
+ReactDOM.render(
     <BrowserRouter>
-    < App / > 
-    </BrowserRouter>
-    
-    , document.getElementById('root'));
+        <App />
+    </BrowserRouter>,
+    document.getElementById('root')
+);
